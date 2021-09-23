b5eeff16629ae7ab928dfdf13585628c92f9d19a
Niels Erik G. Nielsen <nielserik@indexdata.com>
2021-09-23 20:20:29 +0200

    Get query error from Harvester's stack trace
      - and respond with 400 rather than 500

--- src/main/java/org/folio/harvesteradmin/dataaccess/responsehandlers/ProcessedHarvesterResponseGet.java
+++ src/main/java/org/folio/harvesteradmin/dataaccess/responsehandlers/ProcessedHarvesterResponseGet.java
@@ -6,8 +6,13 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
 import org.folio.harvesteradmin.dataaccess.dataconverters.HarvesterXml2Json;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ProcessedHarvesterResponseGet extends ProcessedHarvesterResponse
 {
+    private static final Pattern exceptionDescriptionPattern = Pattern.compile(
+            "(Exception Description:.*?[\\n\\r]+.*?[\\n\\r]+)", Pattern.DOTALL );
     public ProcessedHarvesterResponseGet( AsyncResult<HttpResponse<Buffer>> response, String apiPath, String query )
     {
         if ( response.succeeded() )
@@ -18,6 +23,21 @@ public class ProcessedHarvesterResponseGet extends ProcessedHarvesterResponse
             if ( harvesterStatusCode == 200 )
             {
                 jsonObject = HarvesterXml2Json.convertRecordSetToJson( bodyAsString );
+            }
+            else if ( harvesterStatusCode == 500 && bodyAsString.contains(
+                    "An exception occurred while creating a query in EntityManager" ) )
+            {
+                Matcher m = exceptionDescriptionPattern.matcher( bodyAsString );
+                if ( m.find() )
+                {
+                    errorMessage = "Query failed: " + m.group( 1 ).replaceAll( "&#39;", "'" );
+                }
+                else
+                {
+                    errorMessage = "Query failed: " + bodyAsString;
+                }
+                statusCode = 400;
+                jsonObject = new JsonObject();
             }
             else
             {
