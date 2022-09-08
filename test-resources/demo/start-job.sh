b7e7e3e662b6aa776d2ab6c4c06375fd67f48161
Niels Erik G. Nielsen <nielserik@indexdata.com>
2022-09-08 18:27:58 +0200

    Reflect job-start/stop API changes in schema MODHAADM-5

--- test-resources/demo/start-job.sh
+++ test-resources/demo/start-job.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 ID=$1
 
-if [ -z $token ]
+if [ -z "$token" ]
   then
     echo "Can't find okapi token-- Make sure to run a login script."
     return || exit 
 fi
-if [ -z $ID ]
+if [ -z "$ID" ]
   then
     echo "Usage: . ./start-job.sh [harvestable id]"
     return || exit
 fi
 
+# shellcheck disable=SC2154
 curl -i -w '\n' --http1.1 -X PUT  "$protocol://$host/harvester-admin/jobs/run/$ID"  -H "x-okapi-tenant: $tenant" -H "x-okapi-token: $token" -H "Content-Type: application/json"
