602808583ce92e20909308014ac64a7cc8f72807
Niels Erik Nielsen <nielserik@indexdata.com>
2023-03-15 15:09:39 +0100

    Add dedicated oai-pmh and xml-bulk APIs. Fix harvestable schema (MODHAADM-34)

--- src/main/java/org/folio/harvesteradmin/dataaccess/statics/ApiPaths.java
+++ src/main/java/org/folio/harvesteradmin/dataaccess/statics/ApiPaths.java
@@ -11,6 +11,7 @@ public class ApiPaths {
 
   public static final String HARVESTABLES_PATH = "/harvestables";
   public static final String XML_BULK_PATH = "/xml-bulk";
+  public static final String OAI_PMH_PATH = "/oai-pmh";
   public static final String STORAGES_PATH = "/storages";
   public static final String TRANSFORMATIONS_PATH = "/transformations";
   public static final String STEPS_PATH = "/steps";
@@ -31,6 +32,8 @@ public class ApiPaths {
   public static final String THIS_HARVESTABLES_PATH = THIS_API_BASE_PATH + HARVESTABLES_PATH;
   public static final String THIS_HARVESTABLES_XML_BULK_PATH =
       THIS_API_BASE_PATH + HARVESTABLES_PATH + XML_BULK_PATH;
+  public static final String THIS_HARVESTABLES_OAI_PMH_PATH =
+      THIS_API_BASE_PATH + HARVESTABLES_PATH + OAI_PMH_PATH;
   public static final String THIS_STORAGES_PATH = THIS_API_BASE_PATH + STORAGES_PATH;
   public static final String THIS_TRANSFORMATIONS_PATH = THIS_API_BASE_PATH + TRANSFORMATIONS_PATH;
   public static final String THIS_STEPS_PATH = THIS_API_BASE_PATH + STEPS_PATH;
@@ -44,6 +47,7 @@ public class ApiPaths {
   static {
     harvesterPathByRequestPath.put(THIS_HARVESTABLES_PATH, HARVESTER_HARVESTABLES_PATH);
     harvesterPathByRequestPath.put(THIS_HARVESTABLES_XML_BULK_PATH, HARVESTER_HARVESTABLES_PATH);
+    harvesterPathByRequestPath.put(THIS_HARVESTABLES_OAI_PMH_PATH, HARVESTER_HARVESTABLES_PATH);
     harvesterPathByRequestPath.put(THIS_STORAGES_PATH, HARVESTER_STORAGES_PATH);
     harvesterPathByRequestPath.put(THIS_TRANSFORMATIONS_PATH, HARVESTER_TRANSFORMATIONS_PATH);
     harvesterPathByRequestPath.put(THIS_STEPS_PATH, HARVESTER_STEPS_PATH);
