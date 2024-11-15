47585771eb08d9e92d4efb27310aa44053edc10f
Julian Ladisch <julianladisch@users.noreply.github.com>
2024-11-15 13:37:51 +0100

    MODHAADM-105: Unhandled exception when reading PURGE_LOGS_AFTER
    https://folio-org.atlassian.net/browse/MODHAADM-105
    
    Parsing the response may fail when doing "new JsonObject(response)", "getJsonArray" or "getJsonObject".
    
    The parse error is not handled because the code is in "onSuccess(", therefor the request hangs forever.
    
    Fix:
    
    Use map, not onSuccess.
    
    Avoid Promise that almost always is wrong and can be replaced with Future returning calls.
    
    In addition fix CQL and percent encoding.

--- src/main/java/org/folio/harvesteradmin/foliodata/SettingsClient.java
+++ src/main/java/org/folio/harvesteradmin/foliodata/SettingsClient.java
@@ -1,43 +1,38 @@
 package org.folio.harvesteradmin.foliodata;
 
+import static org.folio.util.StringUtil.cqlEncode;
+
 import io.vertx.core.Future;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.reactivex.core.Promise;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.folio.util.PercentCodec;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
+/**
+ * Run GET /settings/entries HTTP request to get a setting from mod-settings.
+ */
 public class SettingsClient {
-    public static final String SETTINGS_PATH = "/settings/entries";
-    public static final String RECORDS = "items";
+  public static final String SETTINGS_PATH = "/settings/entries";
+  public static final String RECORDS = "items";
 
-    protected static final Logger logger =
-            LogManager.getLogger(SettingsClient.class);
+  protected static final Logger logger =
+      LogManager.getLogger(SettingsClient.class);
 
-    public static Future<String> getStringValue (RoutingContext routingContext, String scope, String key) {
-        String query = "scope==" + scope + " and key==" + key;
-        Promise<String> promise = Promise.promise();
-        Folio.okapiClient(routingContext).get(SETTINGS_PATH +
-                        "?query=(" + URLEncoder.encode(query, StandardCharsets.UTF_8) +")")
-                .onSuccess(response -> {
-                    JsonObject json = new JsonObject(response);
-                    JsonArray entries = json.getJsonArray(RECORDS);
-                    if (entries.isEmpty()) {
-                        promise.complete(null);
+  /**
+   * Get the settings value from mod-settings for the scope and key.
+   */
+  public static Future<String> getStringValue(RoutingContext routingContext,
+      String scope, String key) {
 
-                    } else {
-                        JsonObject entry = entries.getJsonObject(0);
-                        promise.complete(entry.getString("value"));
-                    }
-                }).onFailure(response -> {
-                    logger.error("Could not obtain settings by scope " + scope + " and key " + key + ": " + response.getMessage());
-                    promise.complete(null);
-                });
-        return promise.future();
-    }
+    var cql = "scope==" + cqlEncode(scope) + " and key==" + cqlEncode(key);
+    return Folio.okapiClient(routingContext)
+        .get(SETTINGS_PATH + "?query=" + PercentCodec.encode(cql))
+        .map(response ->
+            new JsonObject(response).getJsonArray(RECORDS).getJsonObject(0).getString("value"))
+        .onFailure(e ->
+            logger.error("Could not obtain settings by scope " + scope
+                + " and key " + key + ": " + e.getMessage()));
+  }
 
 }
